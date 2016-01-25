cf21fceafaa8eb7a031710b8b447bebf8a4b94dd
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-01-25 13:42:11 +0100

    Fixed the automatic server startup for Python 3.

--- features/environment.py
+++ features/environment.py
@@ -1,12 +1,12 @@
-import SimpleHTTPServer
-import SocketServer
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
 from threading import Thread
 
 from behave import *
 
 def before_all(context):
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    context._httpServer = SocketServer.TCPServer(("localhost", 8000), Handler)
+    Handler = SimpleHTTPRequestHandler
+    context._httpServer = TCPServer(("localhost", 8000), Handler)
 
     t = Thread(target=context._httpServer.serve_forever)
     t.start()

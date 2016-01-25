4e0a46af77b511f78e9e0e0290e6ee45f1b622f2
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-01-25 16:40:37 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='germanium',
-    version='1.3.0',
+    version='1.3.1',
     description='The germanium project',
     long_description = 'The germanium project',
     author='Bogdan Mustiata',

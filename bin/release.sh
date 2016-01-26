5127c3915e28c6d4dec8fefa731dff13b4b92a4e
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-01-26 15:05:34 +0100

    Updated the release script for python 3.5

--- bin/release.sh
+++ bin/release.sh
@@ -11,6 +11,8 @@ VERSION=$1
 
 git checkout python3.4
 git rebase -ff master
+git checkout python3.5
+git rebase -ff master
 git checkout python2.7
 git rebase master
 git checkout master
@@ -19,6 +21,8 @@ git push --all -f
 if [[ "$VERSION" != "" ]]; then
     git tag $VERSION master
     git tag "$VERSION-python2.7" python2.7
+    git tag "$VERSION-python3.4" python3.4
+    git tag "$VERSION-python3.5" python3.5
     git push --tags
 fi # [[ "$VERSION" != "" ]]
 

d849e2ad0caf046bf5a64f5db21338ff42db586c
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-02-11 13:47:50 +0100

    Readme update.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='germanium',
-    version='1.3.6',
-    description='The germanium project',
-    long_description = 'The germanium project',
+    version='1.3.7',
+    description='The germanium project.',
+    long_description = 'The germanium project: Selenium WebDriver testing API that doesn\'t suck.',
     author='Bogdan Mustiata',
     author_email='bogdan.mustiata@gmail.com',
     license='BSD',

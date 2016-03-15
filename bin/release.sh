6be88f0fc6369d5d08e1c16c1a9a4443e2661322
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-03-15 11:14:56 +0100

    Docker test matrix.

--- bin/release.sh
+++ bin/release.sh
@@ -9,20 +9,28 @@ set -e
 
 VERSION=$1
 
-git checkout python3.4
-git rebase -ff master
-git checkout python3.5
-git rebase -ff master
-git checkout python2.7
-git rebase master
+PYTHON_VERSIONS="python2.7 python3.4 python3.5"
+
+for PYTHON_VERSION in $PYTHON_VERSIONS; do
+    git checkout $PYTHON_VERSION
+    git rebase -ff master
+
+#    if [[ "$GERMANIUM_RUN_TESTS" != "" ]]; then
+#        docker build -t germanium/germanium-$PYTHON_VERSION -t germanium/germanium .
+#        docker run --rm -it -v $GERMANIUM_FOLDER/features:/tests/features:ro germanium/germanium-$PYTHON_VERSION
+#    fi # [[ "$VERSION" != "" ]]
+done
+
 git checkout master
 git push --all -f
 
 if [[ "$VERSION" != "" ]]; then
     git tag -f $VERSION master
-    git tag -f "$VERSION-python2.7" python2.7
-    git tag -f "$VERSION-python3.4" python3.4
-    git tag -f "$VERSION-python3.5" python3.5
+
+    for PYTHON_VERSION in $PYTHON_VERSIONS; do
+        git tag -f "$VERSION-$PYTHON_VERSION" $PYTHON_VERSION
+    done
+
     git push -f --tags
 
     python setup.py sdist upload -r pypitest

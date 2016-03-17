eacec01279816cf95d9e88950600827acc3239ca
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-03-17 23:13:52 +0100

    Tag specific versions.

--- bin/run-test-matrix.sh
+++ bin/run-test-matrix.sh
@@ -9,7 +9,7 @@ GERMANIUM_FOLDER=$(readlink -f $(dirname $0)/..)
 for PYTHON_VERSION in $PYTHON_VERSIONS; do
     git checkout $PYTHON_VERSION
     git rebase master
-    docker build -t germanium/germanium-$PYTHON_VERSION -t germanium/germanium .
+    docker build -t germanium/germanium-$PYTHON_VERSION .
     docker run --rm -it -v $GERMANIUM_FOLDER/features:/tests/features:ro germanium/germanium-$PYTHON_VERSION
 done
 

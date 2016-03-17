6803ade3019065119ac736375585051d53bf4582
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-03-17 23:45:01 +0100

    Fixed broken import.

--- germanium/static/open_browser.py
+++ germanium/static/open_browser.py
@@ -3,7 +3,7 @@ import re
 from selenium import webdriver
 
 from germanium import GermaniumDriver
-from germanium.GermaniumDriver import DefaultIFrameSelector
+from germanium.iframe_selector import DefaultIFrameSelector
 from .global_germanium_instance import *
 
 

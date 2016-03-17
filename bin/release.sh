e623e8be8583ffc31f39707e4ca4993cd3a4225d
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-03-17 14:15:43 +0100

    Started work on test matrix.

--- bin/release.sh
+++ bin/release.sh
@@ -14,11 +14,6 @@ PYTHON_VERSIONS="python2.7 python3.4 python3.5"
 for PYTHON_VERSION in $PYTHON_VERSIONS; do
     git checkout $PYTHON_VERSION
     git rebase -ff master
-
-#    if [[ "$GERMANIUM_RUN_TESTS" != "" ]]; then
-#        docker build -t germanium/germanium-$PYTHON_VERSION -t germanium/germanium .
-#        docker run --rm -it -v $GERMANIUM_FOLDER/features:/tests/features:ro germanium/germanium-$PYTHON_VERSION
-#    fi # [[ "$VERSION" != "" ]]
 done
 
 git checkout master
---
+++ bin/run-test-matrix.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PYTHON_VERSIONS="python2.7 python3.4 python3.5"
+
+for PYTHON_VERSION in $PYTHON_VERSIONS; do
+    git checkout $PYTHON_VERSION
+    git rebase -ff master
+    docker build -t germanium/germanium-$PYTHON_VERSION -t germanium/germanium .
+    docker run --rm -it -v $GERMANIUM_FOLDER/features:/tests/features:ro germanium/germanium-$PYTHON_VERSION
+done
+

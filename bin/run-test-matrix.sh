0362a54e00bc71fe092f1973621e06485bc63960
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-03-17 22:10:14 +0100

    Removed fast forward in rebase.

--- bin/run-test-matrix.sh
+++ bin/run-test-matrix.sh
@@ -8,7 +8,7 @@ GERMANIUM_FOLDER=$(readlink -f $(dirname $0)/..)
 
 for PYTHON_VERSION in $PYTHON_VERSIONS; do
     git checkout $PYTHON_VERSION
-    git rebase -ff master
+    git rebase master
     docker build -t germanium/germanium-$PYTHON_VERSION -t germanium/germanium .
     docker run --rm -it -v $GERMANIUM_FOLDER/features:/tests/features:ro germanium/germanium-$PYTHON_VERSION
 done

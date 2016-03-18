381985bd6f3f802640ce3c3842bc018a1ce2d90f
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-03-18 10:27:13 +0100

    Added utility to pull all branches.

---
+++ bin/git-pull-all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+git checkout master
+git pull --rebase
+
+PYTHON_VERSIONS="python2.7 python3.4 python3.5"
+
+for PYTHON_VERSION in $PYTHON_VERSIONS; do
+    git checkout $PYTHON_VERSION
+    git reset --hard origin/$PYTHON_VERSION
+done
+
+git checkout master
+

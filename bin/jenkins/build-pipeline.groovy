63c3151421af1aa454790c92641e916e79cbb970
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-03-27 01:36:47 +0100

    test

--- bin/jenkins/build-pipeline.groovy
+++ bin/jenkins/build-pipeline.groovy
@@ -1,4 +1,11 @@
-stage "Test"
+parallel python27: {
+    stage "Python 2.7"
+}, python34: {
+    stage "Python 3.4"
+}, python35: {
+    stage "Python 3.5"
+}, failFast: true
+
 
 node {
     sh '''

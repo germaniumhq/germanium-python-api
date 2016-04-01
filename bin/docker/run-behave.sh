531e064e38600dc126a83081e771a21f021bb8fe
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-04-02 00:10:29 +0200

    Docker work.

--- bin/docker/run-behave.sh
+++ bin/docker/run-behave.sh
@@ -9,7 +9,7 @@ cd /python/bin
 # This will run the tests from the /tests folder.
 
 if [[ $RUN_VNC_SERVER -eq 1 ]]; then
-    echo -e "$VNC_PASSWORD\n$VNC_PASSWORD\n" | vnc4server -geometry 800x600 -depth 16
+    echo -e "$VNC_PASSWORD\n$VNC_PASSWORD\n" | vnc4server -geometry ${VNC_SERVER_WIDTH}x${VNC_SERVER_HEIGHT} -depth ${VNC_SERVER_BPP}
     export DISPLAY=:1
 
     #
@@ -27,4 +27,3 @@ behave --tags ~@no${TEST_BROWSER}
 # kill potentially remaining processes, but don't fail in case
 # stuff can't be killed.
 killall -9 $(ps xu | grep -v "grep " | grep -v bash | grep -v "ps " | grep -v "cut " | grep -v "tr " | tr -s " " | cut -f2 -d\ ) || true
-

b360744bdfc2e9fc509284fbfbc3738e8649bb49
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-06-10 07:48:46 +0200

    Send also the content length.

--- features/PostHttpRequestHandler.py
+++ features/PostHttpRequestHandler.py
@@ -14,5 +14,10 @@ class PostHttpRequestHandler(SimpleHTTPRequestHandler):
 
         filename = form['file'].filename
 
+        body = bytearray("<html><body>Uploaded '%s'.</body><html>" % filename, "utf-8")
+
         self.send_response(200)
-        self.wfile.write(bytearray("<html><body>Uploaded '%s'.</body><html>" % filename, "utf-8"))
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-length", str(len(body)))
+        self.end_headers()
+        self.wfile.write(body)

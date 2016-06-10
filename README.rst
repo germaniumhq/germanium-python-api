4b24dc8fa3b78ddc2163b249515637679a29cf4b
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-06-10 08:19:06 +0200

    Updated the version.

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-germanium 1.7.12
+germanium 1.7.13
 ================
 
 Germanium is a set of extensions on top of the regular WebDriver API, allowing
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='germanium',
-    version='1.7.12',
+    version='1.7.13',
     description='The germanium project: Selenium WebDriver testing API that doesn\'t disappoint.',
     long_description = readme,
     author='Bogdan Mustiata',

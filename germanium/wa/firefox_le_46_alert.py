965267eb70e99e95e0daf0c9d201a94e3f843d7d
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-07-06 22:31:01 +0200

    Fixed wait test.

--- germanium/wa/firefox_le_46_alert.py
+++ germanium/wa/firefox_le_46_alert.py
@@ -3,8 +3,8 @@ from selenium.common.exceptions import UnexpectedAlertPresentException
 
 def _is_firefox_without_marionette(germanium):
     capabilities = germanium.web_driver.capabilities
-    return capabilities['browserName'] == 'Firefox' and \
-           not capabilities['marionette']
+    return capabilities['browserName'].lower() == 'firefox' and \
+           ('marionette' not in capabilities or not capabilities['marionette'])
 
 
 def _alert_exists_firefox(germanium, original_function, *args, **kw):

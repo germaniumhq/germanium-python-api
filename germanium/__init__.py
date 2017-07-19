08bd74fbe5ac9b43a867c8c20e1a34c88443b6b4
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-07-19 22:46:21 +0200

    Revert "Don't patch the alert behavior for Firefox anymore."
    This reverts commit 7ff3d25da97e7d1e5a298ae7303e954095c19b7a.

--- germanium/__init__.py
+++ germanium/__init__.py
@@ -5,3 +5,7 @@ from .util import get_attributes_g
 from .util import click_g, right_click_g, double_click_g, hover_g
 from .util import type_keys_g
 from .util import wait
+
+# patch the capabilities from FIREFOX itself.
+import selenium
+selenium.webdriver.DesiredCapabilities.FIREFOX["unexpectedAlertBehaviour"] = "ignore"

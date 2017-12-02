ed93407cacdbfa4dc3f302c70f43e6b4dedcfa70
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-12-02 23:58:22 +0100

    Use the host network to fetch the pip packages.

--- bin/build-docker-instance.sh
+++ bin/build-docker-instance.sh
@@ -21,8 +21,10 @@ docker build \
     -t germanium/germanium-$TAG_VERSION \
     --build-arg pypi_url=$PIPY_URL \
     --build-arg pypi_index_url=$PIPY_INDEX_URL \
+    --network host \
     .
 
 cd $GERMANIUM_FOLDER/features
 
 docker build -t germanium/germanium-${TAG_VERSION}-tests .
+

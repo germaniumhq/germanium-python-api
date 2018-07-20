77775d6b48eafb85f72f48f016bb3b4f742e7429
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-07-20 23:29:48 +0200

    Updated the release script.

--- bin/release.sh
+++ bin/release.sh
@@ -10,37 +10,28 @@ set -x
 
 VERSION=$1
 
-PYTHON_VERSIONS="python2.7 python3.4 python3.5"
+PYTHON_VERSIONS="python2.7 python3.4 python3.5 python3.6"
+
+git push origin --all
+git push github --all
 
 for PYTHON_VERSION in $PYTHON_VERSIONS; do
-    git checkout $PYTHON_VERSION
-    git rebase master
+    git tag -f "$VERSION-$PYTHON_VERSION" $PYTHON_VERSION
 done
 
+echo '#############################################################################'
+echo '# Pushing Germanium Python 3 to PyPI'
+echo '#############################################################################'
+py 3
 git checkout master
-git push origin --all -f
+python setup.py bdist_wheel upload -r pypitest
+python setup.py bdist_wheel upload -r pypimain
 
-if [[ "$VERSION" != "" ]]; then
-    eval $(archer py)
-
-    git tag -f $VERSION master
-
-    for PYTHON_VERSION in $PYTHON_VERSIONS; do
-        git tag -f "$VERSION-$PYTHON_VERSION" $PYTHON_VERSION
-    done
-
-    git push origin -f --tags
-
-    py 3
-    git checkout python3.5
-    python setup.py bdist_wheel upload -r pypitest
-    python setup.py bdist_wheel upload -r pypimain
-
-    py 2
-    git checkout python2.7
-    python setup.py bdist_wheel upload -r pypitest
-    python setup.py bdist_wheel upload -r pypimain
-
-    git checkout master
-fi # [[ "$VERSION" != "" ]]
+echo '#############################################################################'
+echo '# Pushing Germanium Python 2 to PyPI'
+echo '#############################################################################'
+py 2
+git checkout python2.7
+python setup.py bdist_wheel upload -r pypitest
+python setup.py bdist_wheel upload -r pypimain
 
